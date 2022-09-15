0ec24c3bf5a4e6a0d59b10aab171c9f2a2d1f9af
l-marcel <lmgh1312@gmail.com>
2022-09-15 14:55:55 -0300

    feat: 02 - High Speed

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,34 +1,75 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { MagnifyingGlassPlus } from "phosphor-react";
+import logo from "./assets/logo.svg";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
+    <section className="max-w-[1344px] my-20 mx-auto flex flex-col items-center">
+      <img src={logo} className="max-w-[20%]" alt=""/>
+      
+      <h1 className="text-4xl text-white font-black mt-10">
+        Seu <span className="bg-clip-text bg-nlw-gradient text-transparent ">duo</span> está aqui
+      </h1>
+
+      <main className="grid grid-cols-6 gap-6 mt-14">
+        <a href="#" className="relative overflow-hidden rounded-lg">
+          <img src="lol.png" alt=""/>
+          <div className="w-full pt-16 pb-4 px-4 bg-nlw-game-gradient absolute bottom-0 left-0 right-0">
+            <strong className="font-bold text-white block">League of Legends</strong>
+            <span className="text-zinc-300 text-sm">5 anúncios</span>
+          </div>
         </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+        <a href="#" className="relative overflow-hidden rounded-lg">
+          <img src="apex.png" alt=""/>
+          <div className="w-full pt-16 pb-4 px-4 bg-nlw-game-gradient absolute bottom-0 left-0 right-0">
+            <strong className="font-bold text-white block">Apex Legends</strong>
+            <span className="text-zinc-300 text-sm">5 anúncios</span>
+          </div>
         </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+        <a href="#" className="relative overflow-hidden rounded-lg">
+          <img src="cs.png" alt=""/>
+          <div className="w-full pt-16 pb-4 px-4 bg-nlw-game-gradient absolute bottom-0 left-0 right-0">
+            <strong className="font-bold text-white block">Counter Strike</strong>
+            <span className="text-zinc-300 text-sm">5 anúncios</span>
+          </div>
+        </a>
+        <a href="#" className="relative overflow-hidden rounded-lg">
+          <img src="wow.png" alt=""/>
+          <div className="w-full pt-16 pb-4 px-4 bg-nlw-game-gradient absolute bottom-0 left-0 right-0">
+            <strong className="font-bold text-white block">World of Warcraft</strong>
+            <span className="text-zinc-300 text-sm">5 anúncios</span>
+          </div>
+        </a>
+        <a href="#" className="relative overflow-hidden rounded-lg">
+          <img src="lol.png" alt=""/>
+          <div className="w-full pt-16 pb-4 px-4 bg-nlw-game-gradient absolute bottom-0 left-0 right-0">
+            <strong className="font-bold text-white block">League of Legends</strong>
+            <span className="text-zinc-300 text-sm">5 anúncios</span>
+          </div>
+        </a>
+        <a href="#" className="relative overflow-hidden rounded-lg">
+          <img src="apex.png" alt=""/>
+          <div className="w-full pt-16 pb-4 px-4 bg-nlw-game-gradient absolute bottom-0 left-0 right-0">
+            <strong className="font-bold text-white block">Apex Legends</strong>
+            <span className="text-zinc-300 text-sm">5 anúncios</span>
+          </div>
+        </a>
+      </main>
+
+      <footer className="bg-nlw-gradient pt-1 mt-8 w-full rounded-lg overflow-hidden">
+        <article className="bg-[#2A2634] px-8 py-6 flex justify-between items-center">
+          <div>
+            <h6><strong className="text-2xl text-white font-black">Não encontrou seu duo?</strong></h6>
+            <p className="text-zinc-400">Publique um anúncio para encontrar novos players!</p>
+          </div>
+
+          <button className="py-3 px-4 bg-violet-500 hover:bg-violet-700 text-white rounded flex items-center gap-3">
+            <MagnifyingGlassPlus size={24}/>
+            Publicar anúncio
+          </button>
+        </article>
+      </footer>
+    </section>
+  );
 }
 
-export default App
+export default App;
